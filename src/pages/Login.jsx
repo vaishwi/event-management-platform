d0e467b6360fcb75c7b9d122eedcfd9090c11c4b
Purvesh Rathod <pr306467@dal.ca>
2023-02-23 22:24:34 -0400

    Updated website name

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+    return <div>
+        <h1>Login</h1>
+        <hr/>
+        <p>Set "loggedIn" to <b>true</b> in <b>PrivateRoutes.jsx</b> file to go to the homepage</p>
+    </div>;
 };
 
 export default Login;
--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoutes = () => {
-  const loggedIn = true;
+  const loggedIn = false;
   const defaultNavigatePage = "/login";
   return loggedIn ? <Outlet /> : <Navigate to={defaultNavigatePage} />;
 };
