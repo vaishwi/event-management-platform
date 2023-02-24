4594b414879ce2773add4d0f2af3747fc8be533d
Purvesh Rathod <pr306467@dal.ca>
2023-02-23 21:28:00 -0400

    files added

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import PrivateRoutes from "./utils/PrivateRoutes.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/* Unprotected Routes */}
+        <Route element={<Login />} path="/login" />
+        <Route element={<SignUp />} path="/signup" />
+
+        {/* Protected Routes */}
+        <Route element={<PrivateRoutes />}>
+          <Route element={<Home />} path="/" />
+          <Route element={<Home />} path="/home" />
+          <Route element={<About />} path="/about" />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/PrivateRoutes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoutes = () => {
+  const loggedIn = true;
+  const defaultNavigatePage = "/login";
+  return loggedIn ? <Outlet /> : <Navigate to={defaultNavigatePage} />;
+};
+
+export default PrivateRoutes;
