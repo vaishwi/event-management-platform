85977a77f440c883329f2ad07095744282085899
Vaishwi Patel <vs439755@dal.ca>
2023-02-26 01:56:53 -0400

    private routes modified - Variable introduced for loggedin status

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import {Navigate, Outlet} from "react-router-dom";
-import ResponsiveAppBar from "../components/Navbar";
 
 const PrivateRoutes = () => {
-    const loggedIn = false;
-    localStorage.setItem("loginStatus",false)
+    const loggedIn = true;
+    localStorage.setItem("loginStatus",loggedIn)
     const defaultNavigatePage = "/login";
     return loggedIn ? <Outlet/> : <Navigate to={defaultNavigatePage}/>;
 };
