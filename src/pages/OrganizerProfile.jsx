c21db116109bf2deaed8a132ffa2dc526a511bbb
Vaishwi Patel <vs439755@dal.ca>
2023-02-26 12:04:47 -0400

    Started building Organizer profile

---
+++ src/pages/OrganizerProfile.jsx
@@ -0,0 +1,24 @@
+import { Avatar, ListItem, Stack, Typography } from '@mui/material'
+import { Box } from '@mui/system';
+const OrganizerProfile = () => {
+    return ( 
+        <div>
+            <Stack direction="row">
+                <Box sx={{width: '100', mt:5,ml:5,mr:10,bgcolor: '#efefef'}}>
+                      <Avatar></Avatar>      
+                      <Typography>Hello</Typography>
+                </Box>
+                <Box sx={{width: '200', mt:5,ml:5,mr:10,bgcolor: '#efefef'}}>
+                    <Stack direction="column">
+                        <ListItem>About</ListItem>
+                        <ListItem>Managed By</ListItem>
+                        <ListItem>Occupation</ListItem>
+                        <ListItem></ListItem>
+                    </Stack>               
+                </Box>
+            </Stack>
+        </div>
+    );
+}
+ 
+export default OrganizerProfile;
