a2193d33f3d3d73d1e006d1823912afd1513adbb
DEEP KAMLESHKUMAR DAVE <dp724555@dal.ca>
2023-03-31 17:43:30 -0300

    added getRegisterEvent, getUserRegisterEvents and getEventRegisterUsers apis

--- server/application/business_logic/myEvents.py
+++ server/application/business_logic/myEvents.py
@@ -2,6 +2,8 @@ from fireo.models import Model
 from fireo.fields import TextField, NumberField, DateTime, IDField, BooleanField, ListField, Field
 import requests
 
+from application.business_logic.attendee import Attendee
+
 
 class RegisterEvent(Model):
     id = IDField()
@@ -33,28 +35,25 @@ class RegisterEvent(Model):
             e = RegisterEvent(
                 userID="2",
                 eventID=eventData['id'],
-                price= eventData['price'],
+                price=eventData['price'],
                 paymentMethod='0',
                 count=0
             )
             e.save()
             return e.id
 
-    def get_registered_event(self, key):
-
+    def get_registered_event_by_id(self, key):
+        registered_event_dict = {
+            "success": False,
+            "data": {}
+        }
         try:
-            print("before")
-            response_list = RegisterEvent.collection.filter(eventID=key).fetch()
-            output = [response.to_dict() for response in response_list]
-            if (output):
-                print("return true from check_credential")
-                return True
-            else:
-                print("return false")
-                return False
-        except:
-            return False
-
+            event = RegisterEvent.collection.get(f"register_event/{key}")
+            registered_event_dict['data'] = event.to_dict()
+            registered_event_dict['success'] = True
+        except Exception as e:
+            print(e)
+        return registered_event_dict
 
     def delete_registered_event(self, key):
         print(key)
@@ -71,3 +70,31 @@ class RegisterEvent(Model):
                 return True
         except:
             return False
+
+    def get_registered_event_by_user_id(self, key):
+        registered_events = []
+        try:
+            events = RegisterEvent.collection.filter('userID', '==', key).fetch()
+            registered_events = [event.to_dict() for event in events]
+            return registered_events
+        except Exception as e:
+            print(e)
+        return registered_events
+
+    def get_registered_users_by_event_id(self, key):
+        registered_users = []
+        try:
+            events = RegisterEvent.collection.filter('eventID', '==', key).fetch()
+            registered_events = [event.to_dict() for event in events]
+            for event in registered_events:
+                if "userID" in event:
+                    try:
+                        users = Attendee.collection.filter('id', '==', event["userID"]).fetch()
+                        temp = [u.to_dict() for u in users]
+                        registered_users.append(temp)
+                    except Exception as e:
+                        print(e)
+            return registered_users
+        except Exception as e:
+            print(e)
+        return registered_users
--- server/application/resources/routes.py
+++ server/application/resources/routes.py
@@ -1,7 +1,7 @@
 from application.resources.event_handler import EventEndpoint, EventsEndpoint
 from application.resources.authentication import Authentication
 from application.resources.organizer_handler import OrganizerEndPoint, OrganizersEndpoint, UnauthenticateOrganizerEndPoint
-from application.resources.registerEvent_handler import AddRegisterEvents
+from application.resources.registerEvent_handler import AddRegisterEvents, GetRegisterEvents, GetRegisterUsers
 from application.resources.payment_handler import AddPayment
 from application.resources.registerEvent_handler import DeleteRegisteredEvents
 
@@ -17,6 +17,8 @@ def initialize_routes(api):
     api.add_resource(OrganizersEndpoint,'/organizers')
     api.add_resource(OrganizerEndPoint,'/organizer','/organizer/<id>','/authenticate/<id>')
     api.add_resource(UnauthenticateOrganizerEndPoint,'/unauthOrganizers')
-    api.add_resource(AddRegisterEvents, '/registerEvent', '/registerEvent/<id>')
+    api.add_resource(AddRegisterEvents, '/registerEvent', '/getRegisterEvent/<id>')
+    api.add_resource(GetRegisterEvents, '/getUserRegisterEvents/<id>')
+    api.add_resource(GetRegisterUsers, '/getEventRegisterUsers/<id>')
     api.add_resource(AddPayment, '/addPayment', '/getPayments/<id>')
     api.add_resource(DeleteRegisteredEvents, '/deleteEvent/<id>')
